c43c3d43076df2c7255307b0683e48a44a86fa56
Dylan Crawshaw <dcrawsh@gmail.com>
2020-03-03 21:02:08 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.innerHTML = "Flatiron School is the champion!";
+
+newHeader.id = "victory"
+
+
+
